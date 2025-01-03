b542b32b6adcfdfd3fca63b8913143df89494eb7
Paulo Cândido <paulo.candido@g.globo>
2025-01-03 19:36:03 -0300

    fix: standardize env char case

--- service-b/src/index.js
+++ service-b/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const PORT = process.env.port ?? 3001
+const PORT = process.env.PORT ?? 3001
 
 app.get('/', async (req, res) => {
   console.log('Service B processing request...')
