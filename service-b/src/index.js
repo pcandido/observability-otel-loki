b896aa75c587f7821ad08a036052d22d9e4e34be
Paulo Cândido <paulo.candido@g.globo>
2025-01-03 19:02:14 -0300

    feat: service b

---
+++ service-b/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.port ?? 3000
+
+app.get('/', async (req, res) => {
+  console.log('Service B processing request...')
+
+  const sleepTime = req.query.sleepTime ?? 0
+  await new Promise(res => setTimeout(res, sleepTime))
+
+  const random = Math.round(Math.random() * 100)
+  res.send({ number: random })
+})
+
+app.listen(PORT, () => {
+  console.log(`Service B running at http://localhost:${PORT}`)
+})
