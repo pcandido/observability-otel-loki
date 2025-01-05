7be98a40c20b2cbd2c071f2fd8dd1d0155e6355d
Paulo Cândido <paulo.candido@g.globo>
2025-01-04 23:33:57 -0300

    feat: add trace to a function

--- service-b/src/index.js
+++ service-b/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const logger = require('./logger')
+const { trace } = require('@opentelemetry/api')
 
 const app = express()
 
@@ -10,12 +11,42 @@ const PORT = process.env.PORT ?? 3001
 app.get('/', async (req, res) => {
   logger.info('Service B processing request')
 
+  const sleep1 = Math.random() * 0.4
+  const sleep2 = Math.random() * 0.4
+  const sleep3 = 1 - sleep1 - sleep2
   const sleepTime = req.query.sleepTime ?? 0
-  await new Promise(res => setTimeout(res, sleepTime))
 
-  const random = Math.round(Math.random() * 100)
+  await sleep(sleepTime * sleep1)
+  const random = await generateNumber(sleepTime * sleep2)
+  await sleep(sleepTime * sleep3)
+
   res.send({ number: random })
 })
+
+async function generateNumber(sleepTime) {
+  const tracer = trace.getTracer('default')
+  const span = tracer.startSpan('generateNumber', { attributes: { 'custom.sleepTime': sleepTime } })
+
+  try {
+    span.addEvent('start sleep')
+    await sleep(sleepTime)
+    span.addEvent('end sleep')
+
+    const random = Math.round(Math.random() * 100)
+    span.setAttribute('generated.number', random)
+    span.addEvent('number generated')
+    return random
+  } catch (error) {
+    span.recordException(error)
+    throw error
+  } finally {
+    span.end()
+  }
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 app.listen(PORT, () => {
   logger.info(`Service B running at http://localhost:${PORT}`)
