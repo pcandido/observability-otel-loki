9452a6b47c465e7a7110eaa284e4a60391e0310c
Paulo Cândido <paulo.candido@g.globo>
2025-01-16 20:04:14 -0300

    feat: instrument service-c

--- service-c/src/index.js
+++ service-c/src/index.js
@@ -1,5 +1,6 @@
 const Queue = require('bull')
 const logger = require('./logger')
+const { metrics } = require('@opentelemetry/api')
 
 require('./open-telemetry')
 
@@ -11,11 +12,23 @@ const queue = new Queue(queueName, {
   redis: { host: redisHost, port: redisPort },
 })
 
+const meter = metrics.getMeter('default')
+const freshnessHistogram = meter.createHistogram('event_freshness', {
+  description: 'O tempo entre a criação e o processamento de um evento em milissegundos',
+  unit: 'ms',
+})
+
 queue.process(async (job) => {
   logger.info('Processing job:', job.data)
 
   const sleepTime = job.data.sleepTime ?? 0
   await new Promise((resolve) => setTimeout(resolve, sleepTime))
+
+  const number = job.data.number
+  const createdAt = new Date(job.data.createdAt)
+
+  const freshness = new Date() - createdAt
+  freshnessHistogram.record(freshness, { queue: queueName, number })
 
   logger.info('Job completed:', job.data)
 })
